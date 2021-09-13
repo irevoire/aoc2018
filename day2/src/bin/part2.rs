9ce08790e29431277dfdd41eb8e897e9354fdac8
Tamo <tamo@meilisearch.com>
2021-09-14 00:31:30 +0200

    add day2 part2

---
+++ day2/src/bin/part2.rs
@@ -0,0 +1,24 @@
+use aoc::parser;
+
+/// return the number of letters in common between the two word + a string containing these letters
+fn letter_in_common(base: &str, cmp: &str) -> (usize, String) {
+    let result = base.chars()
+        .zip(cmp.chars())
+        .filter_map(|(base, cmp)| (base == cmp).then(|| base))
+        .collect::<String>();
+
+    (base.len() - result.len(), result)
+}
+
+fn main() {
+    let box_ids = parser::lines_from_args(1).collect::<Vec<_>>();
+
+    for (idx, base_id) in box_ids.iter().enumerate() {
+        for cmp_id in box_ids.iter().skip(idx + 1) {
+            let (nb, result) = letter_in_common(base_id, cmp_id);
+            if nb == 1 {
+                println!("{}", result);
+            }
+        }
+    }
+}
